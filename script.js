5f9eb41bb9675ccca5f3b7ff60fe34c9c51ad169
romaria88 <romaria88@mail.ru>
2022-09-20 19:50:39 +0300

    start project

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-const question = document.querySelector('.main_question');
-
-
-const var1 = document.querySelector('.var1');
-const var2 = document.querySelector('.var2');
-const var3 = document.querySelector('.var3');
-const var4 = document.querySelector('.var4');
-
-
-const varElements = document.querySelectorAll('.var');
-//console.log(varElements);
-
-
-
-
-
-const btn = document.querySelector('.btn');
-
-
-let score = 0;
-const correctAnswer = document.querySelector('.correct_answer');
-const btnCorrect = document.querySelector('.btn_correct');
-
-
 const quizData = [
     {
         question: "Which language runs in a web browser?",
@@ -56,4 +32,63 @@ const quizData = [
         correct: "b",
     },
 ];
+   
+const headerContainer = document.querySelector('.quiz-header');
+const listContainer = document.querySelector('.quiz-list');
+const submitBtn = document.querySelector('.quiz-submit');
+//console.log(headerContainer);
 
+
+let questionIndex = 0;
+const title = document.querySelector('.title');
+const a_answer = document.querySelector('.a_answer');
+const b_answer = document.querySelector('.b_answer');
+const c_answer = document.querySelector('.c_answer');
+const d_answer = document.querySelector('.d_answer');
+
+
+function showQuestion(){
+    //console.log(headerTemplate)
+    title.innerHTML = quizData[questionIndex].question //или через опред const headerTemplate = quizData[questionIndex]
+    a_answer.innerHTML = quizData[questionIndex].a
+    b_answer.innerHTML = quizData[questionIndex].b
+    c_answer.innerHTML = quizData[questionIndex].c
+    d_answer.innerHTML = quizData[questionIndex].d
+}
+showQuestion();
+
+//кнопка Submit и след вопрос
+
+submitBtn.addEventListener('click', nextShowQuestion);
+
+function nextShowQuestion() {
+    for (let i = 0; i <= quizData.length; i++);
+    //
+};
+nextShowQuestion();  
+
+
+let score = 0;
+
+correct_a_answer = document.querySelector('.a_answer').value;
+correct_b_answer = document.querySelector('.b_answer').value;
+correct_c_answer = document.querySelector('.c_answer').value;
+correct_d_answer = document.querySelector('.d_answer').value;
+
+
+if (correct_a_answer == quizData[0].correct) {
+    score += 1;
+}
+if (correct_b_answer == quizData[1].correct) {
+    score += 1;
+}
+if (correct_c_answer == quizData[2].correct) {
+    score += 1;
+}
+if (correct_d_answer == quizData[3].correct) {
+    score += 1;
+}
+
+let result = score;
+document.querySelector('res').innerHTML = 'You answered ' + 'result' + '/4 quiestions correctly';
+console.log(result); 
